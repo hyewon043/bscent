57544e2fbed7264b2c7b9bb3d2bbb52c0f6fe8b9
hyewon043 <121796283+hyewon043@users.noreply.github.com>
2023-01-03 09:39:44 +0900

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,35 @@
+$(function(){
+    $(".is1_center img").click(function(){
+        $(".warp1").fadeIn();
+    });
+  });
+
+  $(function(){
+    $(".X").click(function(){
+        $(".warp1").fadeOut();
+    });
+  });
+
+$(function(){
+  $(".is2_center a").click(function(){
+      $(".warp2").fadeIn();
+  });
+});
+
+$(function(){
+  $(".X").click(function(){
+      $(".warp2").fadeOut();
+  });
+});
+
+$(function(){
+  $(".is3_center a").click(function(){
+      $(".warp2").fadeIn();
+  });
+});
+
+$(function(){
+  $(".X").click(function(){
+      $(".warp3").fadeOut();
+  });
+});
